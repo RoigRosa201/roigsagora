7d1e9ed1f664fca2609d6a5869e949f3a36aefb9
Roig Rosa <roigrosa88@gmail.com>
2023-09-24 01:12:28 -0400

    created the set products bindings

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,8 @@ import Category from './components/Category';
 
 function App() {
   const [categories, setCategories] = useState ([]);
- // const [products, setProducts] = useState ([]);
-
-
+  const [products, setProducts] = useState ([]);
+  
   useEffect(() => {
     //fetch("/categories")
      // fetch("http://localhost:3001/categories")
@@ -19,6 +18,7 @@ function App() {
       // })
       ///console.log(data)
       setCategories(db.categories)
+      setProducts(db.products)
     }, [])
 
     const handleCategoryClick = id => { 
@@ -29,6 +29,12 @@ function App() {
       return categories.map(c =>
         <Category key={c.id} id={c.id} title={c.title}  onCategoryClick={() => handleCategoryClick(c.id)}/>
       );
+    }
+
+    const renderProducts = () => {
+      return products.map(p =>
+        <div key={p.id}>{p.title}</div>
+      )
     }
 
   return (
@@ -42,7 +48,8 @@ function App() {
       
         </nav>
       <article>
-        main area
+        <h1>Products</h1>
+        { products && renderProducts() }
       </article>
       
       </section>
